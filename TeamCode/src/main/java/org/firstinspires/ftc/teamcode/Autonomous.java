67e8ad961e4e1cab6198eac37e8702924581c677
span3 <evanco@protonmail.com>
2020-03-04 16:13:05 -0500

    Added basic classes for TeleOp and Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Autonomous extends LinearOpMode {
+    //All lines in autonomous are run once from top to bottom.
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOp extends OpMode {
+
+    @Override
+    public void init() { //Runs when the user hits init on the phone. Runs once.
+
+    }
+
+    @Override
+    public void loop() {//Runs after the user hits play on the phone. Runs infinitely until the program is stopped.
+
+    }
+}
